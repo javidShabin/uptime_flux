8d796c120060654c8448f5db83a253b6c0382991
javid shabin <javid.main.act@gmail.com>
2025-11-16 19:32:33 +0530

    Create the project controller and service files, export class from the files and import needed moudles

---
+++ apps/api/src/modules/project/project.service.ts
@@ -0,0 +1,18 @@
+import { Types } from "mongoose";
+import { ProjectModel, type IProject } from "./project.model.js";
+import { ProjectError } from "./project.error.js";
+
+import type {
+  CreateProjectInput,
+  UpdateProjectInput,
+  GetProjectsQueryInput,
+} from "./project.schema.js";
+
+
+
+export class ProjectService {
+  constructor() {}
+
+
+}
+
