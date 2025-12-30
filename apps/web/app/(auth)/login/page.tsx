8985fe3739920f2f729ca03fd7f3776acc738336
javid shabin <javid.main.act@gmail.com>
2025-12-30 23:48:18 +0530

    Remove the next.js and install react for building frontned

--- apps/web/app/(auth)/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import LoginForm from "../../../components/auth/LoginForm";
-
-export default function LoginPage() {
-  return (
-    <div style={{ minHeight: "100vh", display: "grid", placeItems: "center" }}>
-      <LoginForm />
-    </div>
-  );
-}
