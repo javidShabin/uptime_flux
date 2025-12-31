aecc247960429ba840e4d1dc7657bf95eb99cdd1
javid shabin <javid.main.act@gmail.com>
2025-12-31 11:52:22 +0530

    Config the axios and intercpetors for request and response, then setu pthe auth api file login and register

---
+++ apps/web/src/api/auth.api.ts
@@ -0,0 +1,15 @@
+import { api } from "./axios";
+
+export async function login(data: { email: string; password: string }) {
+  const res = await api.post("/auth/login", data);
+  return res.data;
+}
+
+export async function register(data: {
+  email: string;
+  password: string;
+  name: string;
+}) {
+  const res = await api.post("/auth/register", data);
+  return res.data;
+}
