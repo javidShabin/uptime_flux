61920d8fb32997ec259d5239c0da038cc2fb9958
javid shabin <javid.main.act@gmail.com>
2026-01-09 07:50:24 +0530

    Fix all extention .js for all files

--- packages/shared/index.ts
+++ packages/shared/index.ts
@@ -1,9 +1,13 @@
+export * from "./monitor/index.js";
+export * from "./incident/index.js";
+export * from "./alert/index.js";
+export * from "./user/index.js";
 
-export * from "./monitor";
-export * from "./incident";
-export * from "./alert";
-export * from "./user";
-
+// Explicit re-exports for better TypeScript resolution
+export { User } from "./user/user.model.js";
+export { Monitor } from "./monitor/monitor.model.js";
+export { Incident } from "./incident/incident.model.js";
+export type { IncidentDTO } from "./incident/incident.types.js";
 
 export interface MonitorJobPayload {
   monitorId: string;
